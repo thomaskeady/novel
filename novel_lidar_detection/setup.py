780ca69cf9d67ea8bc3de6aaee2a9ead0d61870f
rtenaza1 <rtenaza1@jhu.edu>
2019-04-24 20:03:14 -0400

    Added viz node

--- novel_lidar_detection/setup.py
+++ novel_lidar_detection/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
      packages=['novel_lidar_detection'],
-     scripts=['bin/novel_lidar_detection'],
+     scripts=['bin/novel_lidar_detection', 'bin/novel_publish_tf'],
      package_dir={'': 'src'}
 )
 
