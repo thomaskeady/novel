7e735bddb2e6d0ba44b5a6b402b18c7cf825c732
shinaushin <shinaushin@gmail.com>
2019-04-28 13:30:17 -0400

    Added documentation

--- action/src/navigation_actions.cpp
+++ action/src/navigation_actions.cpp
@@ -60,9 +60,22 @@ Action::Action() {
   rotate = {"1", "3", "5", "7", "9", "11", "13", "15", "17"}; // ids to get a better view of
 }
 
+/*
+Determines action to execute based on markers detected
+
+Inputs
+------
+msg: published NovelObjectArray
+
+Output
+------
+
+*/
 void Action::det_callback(const novel_msgs::NovelObjectArray::ConstPtr& msg) {
+  // time msg was created relative to when last trajectory plan finished
   int diff_sec = msg->header.stamp.sec - plan_done.sec;
   int diff_nsec = msg->header.stamp.nsec - plan_done.nsec;
+
   // if we know pose of robot, plan is not currently executing, and msg of detected ids came after plan finished
   if (pose_known && !execute_plan && diff_sec >= 0 && diff_nsec >= 0) {
     int x_run = 0;
@@ -77,28 +90,36 @@ void Action::det_callback(const novel_msgs::NovelObjectArray::ConstPtr& msg) {
     bool should_run = false;
     bool should_rotate = false;
     bool unknown = false;
-
+    bool LIDAR_detected = false;
     for (int i = 0; i < msg->detected_objects.size(); i++) {
-      // convert marker position world coordinate in terms of map grid coordinate
+      // convert marker position world coordinate to map grid coordinate
       int x_coord = (int)round((msg->detected_objects[i].pose.pose.position.x + pose.pose.position.x - map_metadata.origin.position.x)/map_resolution);
       int y_coord = (int)round((msg->detected_objects[i].pose.pose.position.y + pose.pose.position.y - map_metadata.origin.position.y)/map_resolution);
 
       std::string label = msg->detected_objects[i].classification;
-      // depending on label, turn on bool for specific action
-      if (std::find(run.begin(), run.end(), label) != run.end()) {
-        should_run = true;
-        x_run = x_run + x_coord;
-        y_run = y_run + y_coord;
-        run_count = run_count + 1;
-      } else if (std::find(rotate.begin(), rotate.end(), label) != rotate.end()) {
-        should_rotate = true;
-        x_rot = x_rot + x_coord;
-        y_rot = y_rot + y_coord;
-        rot_count = rot_count + 1;
-      } else {
-        unknown = true;
+      // TODO: if LIDAR detection node message (aka msg with empty string as label) detects multiple objs, 
+      //       check if marker is in occupied area of map. Then visit each obj and classify it. 
+      //       After any classification, changes values in occupancy grid so that values in 9x9 area 
+      //       around detected marker become occupied
+      if (label.compare("") == 0) {
+
+      } else if ( sqrt( pow(x_coord,2) + pow(y_coord,2) ) < 5) { // user-defined threshold
+        // depending on label, turn on bool for specific action
+        if (std::find(run.begin(), run.end(), label) != run.end()) {
+          should_run = true;
+          x_run = x_run + x_coord;
+          y_run = y_run + y_coord;
+          run_count = run_count + 1;
+        } else if (std::find(rotate.begin(), rotate.end(), label) != rotate.end()) {
+          should_rotate = true;
+          x_rot = x_rot + x_coord;
+          y_rot = y_rot + y_coord;
+          rot_count = rot_count + 1;
+        } else {
+          unknown = true;
+        }
+        detected_ids.push_back(label);
       }
-      detected_ids.push_back(label);
     }
 
     std::sort(detected_ids.begin(), detected_ids.end()); // sort ids but don't think it's necessary
@@ -145,18 +166,52 @@ void Action::det_callback(const novel_msgs::NovelObjectArray::ConstPtr& msg) {
   }
 }
 
+/*
+Stores map once it is received and shuts down map subscriber
+
+Input
+-----
+msg: occupancy grid map message
+
+Output
+------
+
+*/
 void Action::map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
   map_metadata = msg->info;
   grid = msg->data;
   map_resolution = msg->info.resolution;
   map_known = true;
+  map_sub.shutdown();
+  ROS_INFO("Shut down map subscriber");
 }
 
+/*
+Stores pose of robot
+
+Input
+-----
+msg: pose with covariance of robot sent by amcl
+
+Output
+------
+*/
 void Action::pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
   pose_known = true;
   pose = msg->pose;
 }
 
+/*
+Sends location of where robot should run away to
+
+Input
+-----
+x_run: average x grid coordinate of markers to run away from
+y_run: average y grid coordinate of markers to run away from
+
+Output
+------
+*/
 void Action::runAway(int x_run, int y_run) {
   MoveBaseClient ac("move_base", true);
   while (!ac.waitForServer(ros::Duration(5.0))) {
@@ -206,8 +261,6 @@ void Action::runAway(int x_run, int y_run) {
       int goal_x = index % map_metadata.width; // column
       int goal_y = index / map_metadata.width; // row
 
-      // ROS_INFO_STREAM(weights[goal_y][goal_x]);
-
       // convert goal position to map coordinate from grid coordinate
       goal.target_pose.pose.position.x = goal_x * map_resolution + map_metadata.origin.position.x;
       goal.target_pose.pose.position.y = goal_y * map_resolution + map_metadata.origin.position.y;
@@ -241,6 +294,17 @@ void Action::runAway(int x_run, int y_run) {
   }
 }
 
+/*
+Sends robot to position close to robot and an orientation such that it is facing the marker
+
+Input
+-----
+x_rot: average x grid coordinate of markers to rotate towards
+y_rot: average y grid coordinate of markers to rotate towards
+
+Output
+------
+*/
 void Action::rotateForBetterView(int x_rot, int y_rot) {
   MoveBaseClient ac("move_base", true);
   while (!ac.waitForServer(ros::Duration(5.0))) {
@@ -265,7 +329,7 @@ void Action::rotateForBetterView(int x_rot, int y_rot) {
     int robot_y = (int)round( (y-map_metadata.origin.position.y)) / map_resolution;
 
     double grid_dist = sqrt( pow(robot_x - x_rot, 2) + pow(robot_y - y_rot, 2) ); // current distance between robot and marker
-    double dist_m = grid_dist * map_resolution; // distance in map coordinate
+    double curr_map_dist = grid_dist * map_resolution; // distance in map coordinate
 
     double marker_x = x_rot * map_resolution + map_metadata.origin.position.x; // marker position in map coordinate
     double marker_y = y_rot * map_resolution + map_metadata.origin.position.y;  
@@ -275,7 +339,7 @@ void Action::rotateForBetterView(int x_rot, int y_rot) {
 
     while (execute_plan) {
       // if amcl cannot send robot closer to marker, then keep robot in same place and just rotate
-      if (dist < dist_m) {
+      if (dist < curr_map_dist) {
         goal.target_pose.pose.position.x = x_rot * map_resolution + map_metadata.origin.position.x - dist*cos(angle);
         goal.target_pose.pose.position.y = y_rot * map_resolution + map_metadata.origin.position.y - dist*sin(angle);
         goal.target_pose.pose.orientation.z = angle;
@@ -285,7 +349,7 @@ void Action::rotateForBetterView(int x_rot, int y_rot) {
         goal.target_pose.pose.position.y = robot_y * map_resolution + map_metadata.origin.position.y;
         goal.target_pose.pose.orientation.z = angle;
         goal.target_pose.pose.orientation.w = 1;
-        execute_plan = false; // even if amcl cannot simply rotate robot in place, plan is done
+        execute_plan = false; // even if amcl cannot simply rotate robot in place (for some reason), plan is done
       }
 
       ROS_INFO_STREAM(goal.target_pose.pose.position.x);
@@ -305,7 +369,7 @@ void Action::rotateForBetterView(int x_rot, int y_rot) {
         plan_done = ros::Time::now(); // plan most recently finished
       } else
         ROS_INFO("The base failed to move for some reason. We will sample new goal point.");
-        dist = dist + 0.5;
+        dist = dist + 0.5; // increase distance so robot travels less, leading to (hopefully) easier plan
         if (!execute_plan) {
           plan_done = ros::Time::now(); // plan most recently finished
         }
@@ -313,6 +377,19 @@ void Action::rotateForBetterView(int x_rot, int y_rot) {
   }
 }
 
+/*
+Send location to robot so that it can run away from certain markers and rotate towards other markers
+
+Input
+-----
+x_rot: average x grid coordinate of markers to rotate towards
+y_rot: average y grid coordinate of markers to rotate towards
+x_run: average x grid coordinate of markers to run away from
+y_run: average y grid coordinate of markers to run away from
+
+Output
+------
+*/
 void Action::runAndRotate(int x_rot, int y_rot, int x_run, int y_run) {
   MoveBaseClient ac("move_base", true);
   while (!ac.waitForServer(ros::Duration(5.0))) {
@@ -397,6 +474,20 @@ void Action::runAndRotate(int x_rot, int y_rot, int x_run, int y_run) {
   }
 }
 
+/*
+Construct grid of weights to sample from when deciding where to run to
+
+Input
+-----
+marker_x: average x grid coordinate of markers to run away from
+marker_y: average y grid coordinate of markers to run away from
+robot_x: average x grid coordinate of robot position
+robot_y: average y grid coordinate of robot position
+weights: empty vector of vector of doubles
+
+Output
+------
+*/
 void Action::constructRunGrid(int marker_x, int marker_y, int robot_x, int robot_y, std::vector<std::vector<double>>& weights) {
   // construct grid of weights based on distance from marker location
   // see which indices of grid create obstruction between robot and marker and increase weight for those
@@ -419,7 +510,7 @@ void Action::constructRunGrid(int marker_x, int marker_y, int robot_x, int robot
   ROS_INFO_STREAM(max);
   for (int i = 0; i < map_metadata.height; i++) {
     for (int j = 0; j < map_metadata.width; j++) {
-      // check if this grid index is where marker is and make sure it is accessible
+      // check if this grid index is not where marker is and make sure it is accessible
       if ((j != marker_x) && (i != marker_y) && (grid[i*map_metadata.width + j] < 50) && grid[i*map_metadata.width + j] >= 0 && j != robot_x && i != robot_y) {
         int dist_x = abs(marker_x - j); // distance from marker position
         int dist_y = abs(marker_y - i);
@@ -442,7 +533,6 @@ void Action::constructRunGrid(int marker_x, int marker_y, int robot_x, int robot
             if (grid[ round(y)*map_metadata.width + x ] > 50) {
               double distFromRobot = calcDistance( j, i, robot_x, robot_y);
               weights[i][j] = weights[i][j] + max*5 + longestDist / distFromRobot * 3; // experimental formula
-              // ROS_INFO_STREAM(weights[i][j]);
               break;
             }
             
@@ -475,6 +565,7 @@ void Action::constructRunGrid(int marker_x, int marker_y, int robot_x, int robot
   }
 
   // just for purpose of debugging
+  /*
   for (int i = 0; i < map_metadata.height; i++) {
     for (int j = 0; j < map_metadata.width; j++) {
       if (max < weights[i][j]) {
@@ -483,12 +574,31 @@ void Action::constructRunGrid(int marker_x, int marker_y, int robot_x, int robot
     }
   }
   ROS_INFO_STREAM(max);
+  */
 }
 
+/*
+Construct grid of weights when deciding where robot should be to run away from certain markers and still
+  have a view of the markers robot need to rotate towards
+
+Input
+-----
+x_rot: average x grid coordinate of markers to rotate towards
+y_rot: avearge y grid coordinate of markers to rotate towards
+x_run: average x grid coordinate of markers to run away from
+y_run: average y grid coordinate of markers to run away from
+robot_x: average x grid coordinate of robot position
+robot_y: average y grid coordinate of robot position
+weights: empty vector of vector of doubles
+
+Output
+------
+
+*/
 void Action::constructRunRotGrid(int x_rot, int y_rot, int x_run, int y_run, int robot_x, int robot_y, std::vector<std::vector<double>>& weights) {
   for (int i = 0; i < map_metadata.height; i++) {
     for (int j = 0; j < map_metadata.width; j++) {
-      // check if this grid index is where marker is and make sure it is accessible
+      // check if this grid index is not where average marker positions are  and make sure it is accessible
       if ( j != x_rot && i != y_rot && j != x_run && i != y_run && j != robot_x && i != robot_y && grid[i*map_metadata.width + j] < 50 && grid[i*map_metadata.width + j] >= 0) {
         int dist_x = abs(x_rot - j); // distance from marker position
         int dist_y = abs(y_rot - i);
@@ -551,6 +661,20 @@ void Action::constructRunRotGrid(int x_rot, int y_rot, int x_run, int y_run, int
   }
 }
 
+/*
+Calculate distance between two points
+
+Input
+-----
+x1: x coordinate of point 1
+y1: y coordinate of point 1
+x2: x coordinate of point 2
+y2: y coordinate of point 2
+
+Output
+------
+distance between two 2D points
+*/
 double Action::calcDistance(double x1, double y1, double x2, double y2) {
   double x = x1 - x2;
   double y = y1 - y2;
@@ -565,5 +689,4 @@ int main(int argc, char** argv){
   Action action;
   
   ros::spin();
-  return 0;
 }
