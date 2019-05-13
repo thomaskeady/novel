527ecbd2864d67c728757a25db58131af7012d6d
rtenaza1 <rtenaza1@jhu.edu>
2019-05-12 23:15:59 -0400

    Fixed errors in filter, fixed rviz error, launch file updates

--- expected_scan_generator/src/novel_objects_filter.cpp
+++ expected_scan_generator/src/novel_objects_filter.cpp
@@ -10,6 +10,11 @@ The purpose of this node is to see if novel objects are new or simply redetectio
 #include "novel_msgs/NovelObjectArray.h"
 #include <vector>
 #include <tf/transform_listener.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+
+
+
 class NOFilter 
 {
 private:
@@ -25,16 +30,18 @@ private:
 	ros::Subscriber novel_objects_sub;
 
 	// Other
-	std::vector<novel_msgs::NovelObject> uniques;
+	std::vector<geometry_msgs::Pose> uniques;
     float distance_thresh;
     int8_t id; 
+    std::string fixed_frame;
+    
+    tf::TransformListener &listener_;
 
 public:
-    NOFilter(ros::NodeHandle& nh) :
+    NOFilter(ros::NodeHandle& nh, tf::TransformListener& listener) :
 		nh_(nh),
-		
+		listener_(listener),
 		// Messages
-
 		
 
         // Other
@@ -46,6 +53,8 @@ public:
         nh_.param<float>("distance_thresh", distance_thresh, 0.5);
         nh_.param<std::string>("filtered_objects_topic", out_topic, "filtered_lidar_objects");
         nh_.param<std::string>("lidar_objects_topic", in_topic,"lidar_objects");
+        nh_.param<std::string>("fixed_frame", fixed_frame, "/map");
+
 
         // Publishers
         filtered_novel_objects_pub = nh_.advertise<novel_msgs::NovelObjectArray>(out_topic, 100);
@@ -60,25 +69,45 @@ public:
         // Get coords of new novel objects
         // If close enough to existing objects, consider same & update object position
         // Publish
-	    novel_msgs::NovelObjectArray noa;
+
+
+        novel_msgs::NovelObjectArray noa;
+
         for (int i = 0; i < msg->detected_objects.size(); ++i) 
         {
+            
             bool matched_existing = false;
-            std::vector<novel_msgs::NovelObject>::iterator closest_NO;
+            std::vector<geometry_msgs::Pose>::iterator closest_NO;
             float closest_distance = distance_thresh; // Since one later in the array may be even closer
+            
+            novel_msgs::NovelObject current_obj = msg->detected_objects[i];
 
-            for (std::vector<novel_msgs::NovelObject>::iterator it = uniques.begin() ; it != uniques.end(); it++)
+            geometry_msgs::PoseStamped spose;
+            spose.header = msg->header;
+            spose.pose = current_obj.pose.pose;
+            geometry_msgs::PoseStamped spose_out;
+            
+
+            try{
+                listener_.transformPose(fixed_frame, spose, spose_out);
+            }
+            catch (tf::TransformException &ex) {
+                ROS_ERROR("%s",ex.what());
+            }
+
+            for (std::vector<geometry_msgs::Pose>::iterator it = uniques.begin() ; it != uniques.end(); it++)
             {
-                float dist = sqrt(pow(msg->detected_objects[i].pose.pose.position.x - it->pose.pose.position.x, 2) + pow(msg->detected_objects[i].pose.pose.position.y - it->pose.pose.position.y, 2));
-
-                if (closest_distance > dist)
+                
+                float dist = sqrt(pow(spose_out.pose.position.x - it->position.x, 2) + pow(spose_out.pose.position.y - it->position.y, 2));
+                ROS_INFO("Distance %f", dist);
+                if (dist < closest_distance)
                 {
                     matched_existing = true;
                     closest_NO = it;
                     closest_distance = dist;
                 }
             }
-
+            
             if (!matched_existing) {
                 // Create new object in uniques
                 //msg->detected_objects[i].id = id; // If error cause const, change uniques.back
@@ -86,6 +115,7 @@ public:
                 ROS_INFO("New object detected");
                 noa.detected_objects.push_back(msg->detected_objects[i]);
 		        noa.detected_objects.back().id = id++;
+                uniques.push_back(spose_out.pose);
             } 
             else 
             {
@@ -93,17 +123,13 @@ public:
                 // Update closest_NO
                 // Just take an average for now
                 // Should size or angular_size play a role in this too?
-                try{
-                listener.lookupTransform("/turtle2", "/turtle1",
-                                        ros::Time(0), transform);
-                }
-                catch (tf::TransformException &ex) {
-                ROS_ERROR("%s",ex.what());
-                ros::Duration(1.0).sleep();
-                continue;
-    }
-                closest_NO->pose.pose.position.x = (closest_NO->pose.pose.position.x + msg->detected_objects[i].pose.pose.position.x)/2;
-                closest_NO->pose.pose.position.y = (closest_NO->pose.pose.position.y + msg->detected_objects[i].pose.pose.position.y)/2;
+
+                
+
+                
+    
+                closest_NO->position.x = (closest_NO->position.x + spose_out.pose.position.x)/2;
+                closest_NO->position.y = (closest_NO->position.y + spose_out.pose.position.y)/2;
             }
 
         } // What if something should be removed from this list? Too long without being seen?
@@ -126,13 +152,17 @@ public:
 
 
 };
+
+
+
 int main (int argc, char** argv) 
 {
 	ros::init(argc, argv, "novel_filter");
 	
 	ros::NodeHandle nh;
-	
-	NOFilter f = NOFilter(nh);
+	tf::TransformListener listener;
+
+	NOFilter f = NOFilter(nh, listener);
 
 	ros::Rate r(10);
 	while (ros::ok()) 
